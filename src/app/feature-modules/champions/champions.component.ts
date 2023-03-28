8ab5031a26bc76f61e64888e486d097d0a545cb3
Harold Garcia <hj.garcia652@gmail.com>
2023-03-27 21:19:18 -0500

    search bar

--- src/app/feature-modules/champions/champions.component.ts
+++ src/app/feature-modules/champions/champions.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { switchMap } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, switchMap, tap } from 'rxjs';
 import { DdragonService } from '../services/ddragon.service';
 import { Champion } from './models/champion';
 
@@ -8,11 +8,14 @@ import { Champion } from './models/champion';
   templateUrl: './champions.component.html',
   styleUrls: ['./champions.component.sass']
 })
-export class ChampionsComponent {
+export class ChampionsComponent implements AfterViewInit{
   championsArray: Champion[] = []
   version!: string
   classes: any = {}
   championsNames: string[] = []
+  @ViewChild('input') input!: ElementRef;
+  isClean: boolean = true
+  searchArray: Champion[] = []
 
   constructor(private dDragonService: DdragonService) {
     this.dDragonService.getDDragonDataVersion().pipe(
@@ -27,6 +30,33 @@ export class ChampionsComponent {
           if (!this.classes[x]) this.classes[x] = true
         })
       }
+      this.searchArray = [...this.championsArray]
     })
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(this.input.nativeElement,'keyup')
+            .pipe(
+                filter(Boolean),
+                debounceTime(150),
+                distinctUntilChanged(),
+                tap((text) => {
+                  console.log('text', text)
+                  let texts = (this.input.nativeElement.value || '').toLowerCase()
+                  this.isClean = false
+                  this.searchArray = this.championsArray.filter((x) => x.id.toLowerCase().includes(texts))
+                })
+            )
+            .subscribe();
+  }
+
+  filter(text: string): void {
+  }
+
+
+  cleanField(): void {
+    this.isClean = true
+    this.input.nativeElement.value = ''
+    this.searchArray = [...this.championsArray]
   }
 }
